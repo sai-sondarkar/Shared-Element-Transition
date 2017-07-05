46d0e0e9e0392ded168ef901a15be6aa4e8ac900
sai <saisondarkar18@gmail.com>
2017-07-05 10:04:46 +0530

    shared transtion implemented in one activity

--- app/src/main/java/in/awarespot/sharedelementtransition/MainActivity.java
+++ app/src/main/java/in/awarespot/sharedelementtransition/MainActivity.java
@@ -1,7 +1,12 @@
 package in.awarespot.sharedelementtransition;
 
+import android.content.Intent;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        Button button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+// Pass data object in the bundle and populate details activity.
+                View imageView = (View) findViewById(R.id.imageView);
+                ActivityOptionsCompat options = ActivityOptionsCompat.
+                        makeSceneTransitionAnimation(MainActivity.this, (View)imageView, "profile");
+                startActivity(intent, options.toBundle());
+            }
+        });
+    }}
